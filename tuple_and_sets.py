d6b259322a6b4499eb0595d4dd49796e763d486e
Odhiambo <geoffrey.ochieng.odhiambo03@gmail.com>
2026-01-22 09:06:27 +0300

    Python Tuples

---
+++ tuple_and_sets.py
@@ -0,0 +1,32 @@
+# Tuples
+marks = (70, 85, 90, 75, 60)
+names = ('Amit', 'Deepseek', 'Ravi')
+single = (10,)
+
+print('Min:', min(marks))
+print('Max:', max(marks))
+
+print()
+
+# Accessing names
+for name in names:
+    print(name)
+
+# Access Tuple items
+print(marks[0])
+print(marks[-1])
+
+print()
+
+# Loop through a tuple
+for m in marks:
+    print(m)
+
+print()
+
+# Tuple unpacking
+a, b, c = (10, 20, 30)
+print(a)
+print(b)
+print(c)
+
